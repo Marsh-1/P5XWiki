d3a6ddf075ddb0ee095afe564c422b38f58b5a7b
Marsh-1 <marshgc.business@gmail.com>
2024-06-14 20:29:33 -0400

    Edited config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Marsh-1.github.io',
-  base: 'P5XWiki',
+  site: 'https://Marsh-1.github.io'
 })
